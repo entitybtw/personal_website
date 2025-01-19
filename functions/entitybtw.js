848778b2390bda9090356dc4b449b35a3d764cb6
entitybtw <68101241+entitybtw@users.noreply.github.com>
2025-01-19 12:16:20 +0300

    Update entitybtw.js

--- functions/entitybtw.js
+++ functions/entitybtw.js
@@ -29,7 +29,7 @@ exports.handler = async (event) => {
        =%=     -@=.+==-     :*#
                 =:..         .=.
                               ..
-            `,
+            ` + '\n',
         };
     } else {
         return {
